716d0715afe670d8f7dda0c59c26599c4f67ad13
REVRBE <129604052+REVRBE@users.noreply.github.com>
2023-05-03 13:17:04 +0200

    Update ui.hh (remember color)

--- ui/ui.hh
+++ ui/ui.hh
@@ -19,6 +19,7 @@ namespace ui {
 	inline LPDIRECT3DDEVICE9 dev;
 	inline const char* window_title = "smokescreen.gg";
 	inline const char* colorSchemes[] = { "Standard", "Cinder", "Green colorway", "Blue colorway", "Purple/orange colorway", "Dark mode", "Pink/green/blue colorway", "Blue and grey colorway", "Boring colorway", "Blue/green colorway", "Green/yellow colorway", "Orange/grey colorway", "Whitey", "Colorful"};
+	static int currentColorScheme;
 }
 
 namespace ui {
